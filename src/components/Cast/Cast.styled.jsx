47052b21d9d8bf66925479ad73c6e4c1d9793c4d
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-11-18 00:00:25 +0200

    Update Cast

---
+++ src/components/Cast/Cast.styled.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  margin: 0;
+  padding: 30px 0;
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+export const Image = styled.img`
+  margin-right: 20px;
+`;
+
+export const BoxText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  padding: 20px;
+
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 1;
+  color: black;
+`;
+
+export const Title = styled.h2`
+  margin-top: 0;
+  margin-bottom: 10px;
+
+  font-weight: 600;
+`;
+
+export const Character = styled.p`
+  margin: 0;
+  font-weight: 400;
+`;
