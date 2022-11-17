d1ee8e65ba8f08ba6d6f844136f41e3fc41248e5
ShcherbatayaLuna <105154403+ShcherbatayaLuna@users.noreply.github.com>
2022-11-18 00:00:46 +0200

    Update MovieDetails

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,22 @@
 import { useEffect, useState } from 'react';
-import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import { InfoMovieApi } from '../../utils/MovieApi';
-import { Section, Link, Box } from './MovieDetails.styled';
+import {
+  Section,
+  BackLink,
+  Box,
+  Image,
+  BoxInfo,
+  Title,
+  Score,
+  TitleSecondary,
+  Overview,
+  ListGenres,
+  ItemGenres,
+  Info,
+  ItemLink,
+  Link,
+} from './MovieDetails.styled';
 
 export default function MovieDetails() {
   const [film, setFilm] = useState(null);
@@ -16,45 +31,54 @@ export default function MovieDetails() {
 
   return (
     <Section>
-      <Link to={location.state?.from ?? '/'}>&#8592; Go back</Link>
+      <BackLink to={location.state?.from ?? '/'}>&#8592; Go back</BackLink>
       {film && (
-        <div>
+        <>
           {film.poster_path && (
             <>
               <Box>
-                <img
-                  width="400"
+                <Image
+                  width="300"
+                  height="400"
                   src={`https://image.tmdb.org/t/p/w500/${film.poster_path}`}
                   alt={film.title}
                 />
-                <div>
-                  <h2>{film.title}</h2>
-                  <p>User Score: {film.vote_average * 10}%</p>
-                  <h3>Overview</h3>
-                  <p>{film.overview}</p>
-                  <h3>Genres</h3>
-                  <ul>
+                <BoxInfo>
+                  <Title>{film.title}</Title>
+                  <Score>
+                    User Score: {Math.round(film.vote_average * 10)}%
+                  </Score>
+                  <TitleSecondary>Overview</TitleSecondary>
+                  <Overview>{film.overview}</Overview>
+                  <TitleSecondary>Genres</TitleSecondary>
+                  <ListGenres>
                     {film.genres.map(({ id, name }) => (
-                      <li key={id}>{name}</li>
+                      <ItemGenres key={id}>{name}</ItemGenres>
                     ))}
-                  </ul>
-                </div>
+                  </ListGenres>
+                </BoxInfo>
               </Box>
-              <div>
-                <h4>Additional information</h4>
-                <Link to="cast" state={{ from: location.state.from }}>
-                  {''}
-                  Cast{''}
-                </Link>
-                <Link to="reviews" state={{ from: location.state.from }}>
-                  {''}
-                  Reviews{''}
-                </Link>
-                <Outlet />
-              </div>
+              <Info>
+                <TitleSecondary>Additional information</TitleSecondary>
+                <ul>
+                  <ItemLink>
+                    <Link to="cast" state={{ from: location.state.from }}>
+                      {''}
+                      Cast{''}
+                    </Link>
+                  </ItemLink>
+                  <li>
+                    <Link to="reviews" state={{ from: location.state.from }}>
+                      {''}
+                      Reviews{''}
+                    </Link>
+                  </li>
+                </ul>
+              </Info>
+              <Outlet />
             </>
           )}
-        </div>
+        </>
       )}
     </Section>
   );
--- src/components/MovieDetails/MovieDetails.styled.jsx
+++ src/components/MovieDetails/MovieDetails.styled.jsx
@@ -1,10 +1,103 @@
 import styled from 'styled-components';
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Section = styled.section`
-  padding: 30px 15px;
+  padding: 30px 0;
+`;
+
+export const BackLink = styled(NavLink)`
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 1;
+  text-decoration: none;
+  color: black;
+
+  :hover,
+  :focus {
+    color: red;
+  }
 `;
 
 export const Box = styled.div`
-display: flex;
-`
+  display: flex;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  box-shadow: 0px 4px 4px -4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Image = styled.img`
+  margin-right: 20px;
+`;
+
+export const BoxInfo = styled.div`
+  padding: 20px 0;
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 1;
+  color: black;
+`;
+
+export const Title = styled.h2`
+  margin-top: 0;
+  margin-bottom: 20px;
+  font-weight: 700;
+`;
+
+export const Score = styled.p`
+  margin-top: 0;
+  margin-bottom: 15px;
+  font-weight: 400;
+`;
+
+export const TitleSecondary = styled.h3`
+  margin-top: 0;
+  margin-bottom: 15px;
+  font-weight: 600;
+`;
+
+export const Overview = styled.p`
+  margin-top: 0;
+  margin-bottom: 15px;
+  font-weight: 400;
+`;
+
+export const ListGenres = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+export const ItemGenres = styled.li`
+  margin-right: 15px;
+`;
+
+export const Info = styled.div`
+  padding-top: 15px;
+  padding-bottom: 15px;
+  box-shadow: 0px 4px 4px -4px rgba(0, 0, 0, 0.25);
+
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 1;
+  color: black;
+`;
+
+export const ItemLink = styled.li`
+  margin-bottom: 15px;
+`;
+
+export const Link = styled(NavLink)`
+  font-weight: 400;
+  text-decoration: none;
+  color: black;
+
+  :hover,
+  :focus {
+    color: red;
+  }
+`;
